c0318be9d5e7669ab46d4fbddcc8e37c72655ce1
dn200978lak <dn200978lak@gmail.com>
2015-03-12 12:18:48 +0200

    0.0.0

---
+++ api/v1/config.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Database configuration
+ */
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '1111');
+define('DB_HOST', '10.13.71.153');
+define('DB_NAME', 'angularcode_products');
+
+?>
